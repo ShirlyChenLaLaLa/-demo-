f173b3c651137ce07b9c520951d7cfcd7b69d93b
Shirly <1015885915@qq.com>
2019-01-17 17:30:29 +0800

    graphql-tutorial

---
+++ react-native-demo/.history/ApolloComponent_20190116110326.js
@@ -0,0 +1,16 @@
+export default class App {
+    createClient() {
+      return new ApolloClient({
+        networkInterface: createNetworkInterface({
+          uri: 'http://api.githunt.com/graphql',
+        }),
+      });
+    }
+    render() {
+      return (
+        <ApolloProvider client={this.createClient()}>
+          <FeedWithData />
+        </ApolloProvider>
+      );
+    }
+  }
