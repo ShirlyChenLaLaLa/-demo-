21a65e261cf1246df8ebf4f9e47195774085ec68
Shirly <1015885915@qq.com>
2019-02-22 19:02:38 +0800

    git ignore

--- sub/fultter_app/lib/main.dart
+++ sub/fultter_app/lib/main.dart
@@ -1,31 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Welcome tooooooo Flutter'),
+          title: Text('Welcome to Flutter'),
         ),
         body: Center(
-          child: Text(wordPair.asPascalCase),
+          child: Text('Hello World'),
         ),
       ),
     );
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  RandomWordsState createState() => new RandomWordsState();
-  Widget build(BuildContext context) {
-    final WordPair wordPair = new WordPair.random();
-    return new Text(wordPair.asPascalCase);
   }
 }
